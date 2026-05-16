ed3162872c75502506aebbe8bc1d685ecd92d705
mao20260306 <asaasa0220@gmail.com>
2026-05-16 14:06:02 +0900

    Delete download_onedrive_csv.py

--- download_onedrive_csv.py
+++
@@ -1,9 +0,0 @@
-import os
-import requests
-
-url = os.environ["ONEDRIVE_URL"]
-r = requests.get(url)
-with open("テスト.csv", "wb") as f:
-    f.write(r.content)
-
-print("CSV downloaded successfully")
